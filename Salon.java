8311c9e183b58e0122d328d612b45c76e4c1ca40
Jimaa07 <mor22781@uvg.edu.gt>
2025-07-31 07:49:55 -0600

    yo jair hice otros cambios

---
+++ Salon.java
@@ -0,0 +1,50 @@
+
+
+public class Salon {
+    // Atributos
+    private String nombre;
+    private boolean disponible;
+    private double tamaño;
+    private String genero;
+
+    // Constructores
+    public Salon() {
+        this.nombre = "";
+        this.disponible = true;
+        this.tamaño = 0.0;
+        this.genero = "";
+    }
+
+    public Salon(String nombre, boolean disponible) {
+        this.nombre = nombre;
+        this.disponible = disponible;
+        this.tamaño = 0.0;
+        this.genero = "";
+    }
+
+    public Salon(String nombre, boolean disponible, double tamaño, String genero) {
+        this.nombre = nombre;
+        this.disponible = disponible;
+        this.tamaño = tamaño;
+        this.genero = genero;
+    }
+
+    // Métodos
+    public String getTamaño() {
+        return tamaño + " m²";
+    }
+
+    public void limpiar() {
+        System.out.println("El salón " + nombre + " ha sido limpiado.");
+    }
+
+    public void cambiarDisponibilidad() {
+        disponible = !disponible;
+        System.out.println("La disponibilidad del salón " + nombre + " ahora es: " + (disponible ? "Disponible" : "No disponible"));
+    }
+
+    public void mostrarTamaño() {
+        System.out.println("El tamaño del salón " + nombre + " es de " + tamaño + " m².");
+    }
+}
+
--- controlador/controladorgimnasio.java
+++ controlador/controladorgimnasio.java
@@ -1,5 +1,10 @@
 package controlador;
 import ClasesModelo.*;
-public class controladorgimnasio {
+public class Controladorgimnasio {
     
 }
+public void ObtenerGenero(boolean Genero){
+        this.Genero = Genero; }
+
+public void ObtenerDisponibilidad(boolean Disponibilidad){
+        this.Disponibilidad = Disponibilidad;}
