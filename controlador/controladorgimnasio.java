1015733c9f95d550f10fd1c4f4162ba3b51746e8
Jimaa07 <mor22781@uvg.edu.gt>
2025-07-31 07:56:16 -0600

    edite el constructor

--- controlador/controladorgimnasio.java
+++ controlador/controladorgimnasio.java
@@ -2,24 +2,20 @@ package controlador;
 import ClasesModelo.*;
 import java.util.Scanner;
 
-import javax.swing.text.StyledEditorKit.BoldAction;
-
-
 
 public class controladorgimnasio {
     Salon salon;
 
-    Scanner scanner = new Scanner(System.in);  
-    System.out.print("Introduce el nombre del salón: "); 
-    String nombre = scaner
-
-
-
-
-
-}
+    Scanner scanner = new Scanner(System.in); 
+    
+    controladorgimnasio(){
+        }
+    
 public void ObtenerGenero(boolean Genero){
-        this.Genero = Genero; }
+        this.Genero = Genero; 
+}
 
 public void ObtenerDisponibilidad(boolean Disponibilidad){
-        this.Disponibilidad = Disponibilidad;}
+        this.Disponibilidad = Disponibilidad;
+}
+}
